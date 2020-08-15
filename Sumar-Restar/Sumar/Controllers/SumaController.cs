a8fa0cc6339f1cc1df37a4cd9965e7aac9c85724
AnahiSamanthaAgredaCruz <a201911877@estudiantes.upsa.edu.bo>
2020-08-15 14:27:34 -0400

    Se agrego Multiply, en el Suma Controller

--- Sumar-Restar/Sumar/Controllers/SumaController.cs
+++ Sumar-Restar/Sumar/Controllers/SumaController.cs
@@ -11,15 +11,29 @@ namespace Sumar.Controllers
     [ApiController]
     public class SumaController : ControllerBase
     {
-        [HttpGet]
+        [HttpGet]//Se arma la ruta
+        [Route("Add")]
         public int Add(int a, int b)
         {
             return a + b;
         }
         [HttpPost]
+        [Route("Add")]
         public int Add2([FromHeader]int a,[FromHeader] int b)
         {
             return a + b;
+        }
+        [HttpGet]
+        [Route("Multiply")]
+        public int Mul(int a, int b)
+        {
+            return a * b;
+        }
+        [HttpPost]
+        [Route("Multiply")]
+        public int Mul2([FromHeader]int a, [FromHeader]int b)
+        {
+            return a * b;
         }
     }
 }
